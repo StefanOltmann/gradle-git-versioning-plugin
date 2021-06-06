4104d50c55abff6aae428ed1b5434343b8e79cf8
Stefan Oltmann <github@stefan-oltmann.de>
2021-06-06 10:13:56 +0200

    Set version from task

--- src/main/kotlin/de/stefan_oltmann/GradleGitVersioning.kt
+++ src/main/kotlin/de/stefan_oltmann/GradleGitVersioning.kt
@@ -1,7 +1,10 @@
 package de.stefan_oltmann
 
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
 import java.io.File
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -9,20 +12,27 @@ import java.time.ZoneId
 @Suppress("unused")
 class GradleGitVersioning: Plugin<Project> {
 
+    private lateinit var project: Project
+
     override fun apply(project: Project) {
+
+        this.project = project
 
         val task = project.tasks.create("setVersionFromGit") { task ->
             task.doLast {
-                setVersionFromGit(project.rootProject.projectDir)
+
+                val versionName = generateVersioName(project.rootProject.projectDir)
+
+                versionName?.let { project.version = it }
             }
         }
 
         task.group = "versioning"
     }
 
-    private fun setVersionFromGit(projectDir: File) {
+    private fun generateVersioName(projectDir: File): String? {
 
-        val commitTime = GitUtil.getCommitTime(projectDir) ?: return
+        val commitTime = GitUtil.getCommitTime(projectDir) ?: return null
 
         val dateTime = LocalDateTime.ofInstant(
             commitTime,
@@ -32,5 +42,7 @@ class GradleGitVersioning: Plugin<Project> {
         val versionName = VersionNameGenerator.generateVersionName(dateTime)
 
         println("Commited on $dateTime. This results in version $versionName")
+
+        return versionName
     }
  }
